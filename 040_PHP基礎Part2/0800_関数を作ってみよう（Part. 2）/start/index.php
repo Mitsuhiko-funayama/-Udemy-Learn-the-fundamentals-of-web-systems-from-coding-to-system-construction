aa6e7e103166ebaaea4e5a304aa73f777e8a127a
Mitsuhiko-funayama <akauntoshishiyong2022@gmail.com>
2026-03-19 10:47:04 +0900

    2026_03_19_関数を作ってみよう（Part.2）_追記

--- 040_PHP基礎Part2/0800_関数を作ってみよう（Part. 2）/start/index.php
+++ 040_PHP基礎Part2/0800_関数を作ってみよう（Part. 2）/start/index.php
@@ -13,9 +13,13 @@ function with_tax($base_price, $tax_rate = 0.1) { // デフォルト引数を設
     $sum_price = round($sum_price); // 他に引数を設定しない場合は、四捨五入する
     return $sum_price;
 }
-$price1 = with_tax($price, 0.08); // デフォルト引数があっても、引数設定すればこちらが優先される
-echo $price1 . '<br>';
+// $price1 = with_tax($price, 0.08); // デフォルト引数があっても、引数設定すればこちらが優先される
+// echo $price1 . '<br>';
 
-$price2 = 'with_tax'($price, 0.08); // PHPでは文字列を関数として実行することも可能
-echo $price2 . '<br>';
-print_r($price1) . '<br>'; //echo()と書いてしまうとブラウザ上で[1]が表示されてしまうので、echo()はいらない
+// $price2 = 'with_tax'($price, 0.08); // PHPでは文字列を関数として実行することも可能
+// echo $price2 . '<br>';
+// print_r($price1) . '<br>'; //echo()と書いてしまうとブラウザ上で[1]が表示されてしまうので、echo()はいらない
+
+$fn = 'with_tax'; // 文字列を変数に入れることができる
+$price = $fn($price, 0.08);
+echo $price;
