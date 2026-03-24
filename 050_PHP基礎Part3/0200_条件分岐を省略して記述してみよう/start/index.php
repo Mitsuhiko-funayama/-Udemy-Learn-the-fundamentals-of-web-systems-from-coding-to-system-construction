827c4633beb97099ea8a7370e51f2c7b802ac520
Mitsuhiko-funayama <akauntoshishiyong2022@gmail.com>
2026-03-24 18:36:19 +0900

    2026_03_24_条件分岐を省略して記述してみよう

--- 050_PHP基礎Part3/0200_条件分岐を省略して記述してみよう/start/index.php
+++ 050_PHP基礎Part3/0200_条件分岐を省略して記述してみよう/start/index.php
@@ -1,1 +1,32 @@
 <?php
+/***
+ * 条件分岐を省略して記述しよう
+ *
+ * 三項演算子の使い方
+ * null合体演算子
+ */
+$array = [
+    'key' => 10,
+];
+
+// if文を使った書き方
+// if (isset($array['key'])) {
+//     $array['key'] = $array['key'] * 10 . '<br>';
+//  // $array['key'] *= 10; // <-上記と書いてある内容は同じ！
+// } else {
+//     $array['key'] = 1;
+// }
+// echo $array['key'];
+
+// 三項演算子を使った書き方
+// $array['key'] = isset($array['key']) ? $array['key'] * 10 : 1;
+// echo $array['key'];
+
+// null合体演算子
+if (isset($array['key'])) {
+} else {
+     $array['key'] = 1;
+}
+$array['key'] = $array['key'] ?? 1; // ??の部分がnullの部分になる
+echo $array['key'];
+
