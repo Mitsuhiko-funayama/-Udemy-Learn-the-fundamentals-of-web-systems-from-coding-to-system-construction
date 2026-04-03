3d1cf132c4e1a28f5f70a7ca5d9ee513823a81d6
Mitsuhiko-funayama <akauntoshishiyong2022@gmail.com>
2026-04-03 13:20:38 +0900

    2026_04_03_クラス継承（Part1）

--- 050_PHP基礎Part3/1000_クラス継承（Part1）/start/index.php
+++ 050_PHP基礎Part3/1000_クラス継承（Part1）/start/index.php
@@ -1,12 +1,9 @@
 <?php
-/**
- * クラス継承
- */
 class Person
 {
-    private $name;
+    protected $name;
     public $age;
-    public const WHERE = 'Earth';
+    public static $WHERE = 'Earth';
 
     function __construct($name, $age)
     {
@@ -24,8 +21,35 @@ class Person
     }
 }
 
-$bob = new Person('Bob', 18);
-$bob->hello();
+/** 継承とは？
+ * Personクラスからextendsでクラスを継承した、
+ * Japaneseクラスを作成する
+ */
+class Japanese extends Person {
+
+    // public static $WHERE = '日本';
+
+    function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = '30歳';
+    }
+
+    function hello() {
+        echo 'こんにちは、 ' . $this->name . '<br>';
+        return $this;
+    }
+
+        function address() {
+        echo '住所は、 ' . static::$WHERE . 'です。<br>';
+        return $this;
+    }
+}
+
+$taro = new Japanese('太郎', 18);
+$taro->hello();
+$taro->address();
+echo $taro->age;
 // $bob->hello()->bye();
 
 // $tim = new Person('Tim', 32);
