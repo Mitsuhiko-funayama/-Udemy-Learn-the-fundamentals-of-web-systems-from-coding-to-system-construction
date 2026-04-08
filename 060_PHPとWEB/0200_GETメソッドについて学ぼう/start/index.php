91fd23b8f112d6a5f25d55837d267c0705df4100
Mitsuhiko-funayama <akauntoshishiyong2022@gmail.com>
2026-04-08 12:16:33 +0900

    2026_04_08_GETメソッドについて学ぼう

---
+++ 060_PHPとWEB/0200_GETメソッドについて学ぼう/start/index.php
@@ -0,0 +1,11 @@
+<!--
+    現在の階層と同じところにあるファイルにリクエストを送信する：相対パス
+    fullstack-webdev/060_PHPとWEB/0200_GETメソッドについて学ぼう/start/index.php：絶対パス
+    -> 階層を全部指定するのが絶対パス
+-->
+<form action="get.php" method="GET">
+
+    <input type="text" name="username">
+    <input type="password" name="pwd">
+    <input type="submit" value="ボタンを押してね">
+</form>
