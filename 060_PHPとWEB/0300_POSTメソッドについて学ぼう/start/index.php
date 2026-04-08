11f91483b60020f9a895f58eee06c5241a6a3cbf
Mitsuhiko-funayama <akauntoshishiyong2022@gmail.com>
2026-04-08 13:32:42 +0900

    2026_04_08_POSTメソッドについて学ぼう

--- 060_PHPとWEB/0300_POSTメソッドについて学ぼう/start/index.php
+++ 060_PHPとWEB/0300_POSTメソッドについて学ぼう/start/index.php
@@ -1,4 +1,7 @@
-<form action="">
+<!--
+POSTメソッドはパラメーターをボディ部分に格納する
+-->
+<form action="post.php" method="POST"> 
     <input type="text" name="username">
     <input type="password" name="pwd">
     <input type="submit" value="ボタンを押してね">
