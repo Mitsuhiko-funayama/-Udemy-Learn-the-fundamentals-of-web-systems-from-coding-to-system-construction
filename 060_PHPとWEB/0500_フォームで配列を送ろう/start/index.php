f83932c040cf0917b7cee392e05ec1eeef6f7daf
Mitsuhiko-funayama <akauntoshishiyong2022@gmail.com>
2026-04-08 18:12:48 +0900

    2026_04_08_フォームで配列を送ろう

---
+++ 060_PHPとWEB/0500_フォームで配列を送ろう/start/index.php
@@ -0,0 +1,13 @@
+<form action="receive.php">
+    <div>
+        <!-- nameプロパティは配列や連想配列の様に使うことができる -->
+        <input type="text" name="members[]">
+    </div>
+    <div>
+        <input type="text" name="members[]">
+    </div>
+    <div>
+        <input type="text" name="members[]">
+    </div>
+    <input type="submit">
+</form>
